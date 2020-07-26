7d326064352e9ab38825803b7af32fcd9c1d2d18
laitsky <vincent.diamond15@gmail.com>
2020-07-26 11:26:56 +0700

    fixing vscode emphasized items

--- src/views/auth/DaftarMaba/DaftarMaba.js
+++ src/views/auth/DaftarMaba/DaftarMaba.js
@@ -6,12 +6,13 @@ import logo from '../../../assets/mxm20_logo.png';
 import { MxmInput } from '../../../components/reusable/input';
 import { MxmButton } from '../../../components/reusable/button';
 import { Error } from '../../../components/reusable/error';
-import { MxmLogoContainer, AlignMiddle } from '../../../components/reusable/container';
+import {
+  MxmLogoContainer,
+  AlignMiddle,
+} from '../../../components/reusable/container';
 
 const DaftarMaba = () => {
-  const {
-    register, handleSubmit, reset, errors, watch,
-  } = useForm();
+  const { register, handleSubmit, reset, errors, watch } = useForm();
   const password = useRef({});
   password.current = watch('password', '');
   const onSubmit = (data) => {
@@ -26,57 +27,77 @@ const DaftarMaba = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <AlignMiddle>
           <MxmLogoContainer src={logo} alt="MAXIMA 2020 Logo" />
-          <h1 className="mxm-navy" style={{ textAlign: 'center', fontFamily: 'canaro-bold' }}>PENDAFTARAN AKUN BARU MAXIMA 2020</h1>
+          <h1
+            className="mxm-navy"
+            style={{ textAlign: 'center', fontFamily: 'canaro-bold' }}
+          >
+            PENDAFTARAN AKUN BARU MAXIMA 2020
+          </h1>
           <MxmInput
             type="text"
             name="nama"
             placeholder="Nama Lengkap"
             ref={register({ required: 'Isi nama lengkap kamu' })}
           />
-          { errors.nama && <Error>{errors.nama.message}</Error> }
+          {errors.nama && <Error>{errors.nama.message}</Error>}
           <MxmInput
             type="text"
             name="nim"
             placeholder="NIM"
-            ref={register({ required: 'Isi Nomor Induk Mahasiswa kamu' })}
+            ref={register({
+              required: 'Isi Nomor Induk Mahasiswa kamu',
+            })}
           />
-          { errors.nim && <Error>{errors.nim.message}</Error> }
+          {errors.nim && <Error>{errors.nim.message}</Error>}
           <MxmInput
             type="text"
             name="jurusan"
             placeholder="Jurusan"
             ref={register({ required: 'Isi jurusan kamu' })}
           />
-          { errors.jurusan && <Error>{errors.jurusan.message}</Error> }
+          {errors.jurusan && <Error>{errors.jurusan.message}</Error>}
           <MxmInput
             type="text"
             name="email"
             placeholder="Email"
             ref={register({ required: 'Isi email kamu' })}
           />
-          { errors.email && <Error>{errors.email.message}</Error> }
+          {errors.email && <Error>{errors.email.message}</Error>}
           <MxmInput
             type="password"
             name="password"
             placeholder="Kata Sandi"
             ref={register({ required: 'Isi kata sandi kamu' })}
           />
-          { errors.password && <Error>{errors.password.message}</Error> }
+          {errors.password && (
+            <Error>{errors.password.message}</Error>
+          )}
           <MxmInput
             type="password"
             name="password_conf"
             placeholder="Ulangi Kata Sandi"
             ref={register({
-              validate: (val) => val === password.current || 'Kata sandi tidak sama',
+              validate: (val) =>
+                val === password.current || 'Kata sandi tidak sama',
             })}
           />
-          { errors.password_conf && <Error>{errors.password_conf.message}</Error> }
+          {errors.password_conf && (
+            <Error>{errors.password_conf.message}</Error>
+          )}
           <MxmButton mt="1.5em">Daftar</MxmButton>
           <Box mt={4}>
-            <Link to="/login" exact style={{ textDecoration: 'none' }}>
+            <Link
+              to="/login"
+              exact
+              style={{ textDecoration: 'none' }}
+            >
               <h4 className="mxm-cyan">
                 Sudah memiliki akun?
-                <span style={{ fontFamily: 'canaro-bold', marginLeft: 5 }}>Masuk</span>
+                <span
+                  style={{ fontFamily: 'canaro-bold', marginLeft: 5 }}
+                >
+                  Masuk
+                </span>
               </h4>
             </Link>
           </Box>
