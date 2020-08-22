0b4af15e9978857967039e48b0ed8af627198486
olwennnn <j.olwennnn@users.noreply.github.com>
2020-08-22 15:38:00 +0700

    animate

--- src/views/home/HomePuzzle/HomePuzzle.js
+++ src/views/home/HomePuzzle/HomePuzzle.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { Box } from '@material-ui/core';
+import { motion } from 'framer-motion';
 import {
   MxmHomeCard,
   MxmHomeCardDeck,
@@ -19,156 +20,256 @@ const useStyles = makeStyles({
 const HomePuzzle = () => {
   const classes = useStyles();
   return (
-    <Box
-      bgcolor="#1F2C4C"
-      minHeight="100vh"
-      paddingLeft="20px"
-      paddingRight="20px"
+    <motion.div
+      initial={{ y: 999 }}
+      animate={{ y: 0 }}
+      transition={{
+        type: 'spring',
+        stiffness: 100,
+        damping: 20,
+      }}
     >
-      <h1
-        className="mxm-yellow"
-        style={{
-          fontFamily: 'canaro-bold',
-          letterSpacing: 1.5,
-          textAlign: 'center',
-          paddingTop: '1.5em',
-          margin: 0,
-        }}
+      <Box
+        bgcolor="#1F2C4C"
+        minHeight="100vh"
+        paddingLeft="20px"
+        paddingRight="20px"
       >
-        Pilih kategori organisasi yang ingin kamu cari.
-      </h1>
-      <MxmHomeCardDeck>
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/ukm-sains-dan-sosial"
-            className={classes.link}
+        <h1
+          className="mxm-yellow"
+          style={{
+            fontFamily: 'canaro-bold',
+            letterSpacing: 1.5,
+            textAlign: 'center',
+            paddingTop: '1.5em',
+            margin: 0,
+          }}
+        >
+          Pilih kategori organisasi yang ingin kamu cari.
+        </h1>
+        <MxmHomeCardDeck>
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 0.2,
+            }}
           >
-            <MxmHomeCard color="#F4224B" border="#F4224B">
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#FFD008' }}>
-                  KOINONIKES
-                  <br />
-                  EPTIMONIKES
-                </h3>
-                <span>(UKM Sains & Sosial)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/ukm-sains-dan-sosial"
+                className={classes.link}
+              >
+                <MxmHomeCard color="#F4224B" border="#F4224B">
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#FFD008' }}>
+                      KOINONIKES
+                      <br />
+                      EPTIMONIKES
+                    </h3>
+                    <span>(UKM Sains & Sosial)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/ukm-seni-dan-budaya"
-            className={classes.link}
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 0.4,
+            }}
           >
-            <MxmHomeCard>
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#1F2C4C' }}>TECHNI</h3>
-                <span>(UKM Seni & Budaya)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/ukm-seni-dan-budaya"
+                className={classes.link}
+              >
+                <MxmHomeCard>
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#1F2C4C' }}>TECHNI</h3>
+                    <span>(UKM Seni & Budaya)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/ukm-olahraga"
-            className={classes.link}
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 0.6,
+            }}
           >
-            <MxmHomeCard>
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#1F2C4C' }}>ATHLIMA</h3>
-                <span>(UKM Olahraga)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/ukm-olahraga"
+                className={classes.link}
+              >
+                <MxmHomeCard>
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#1F2C4C' }}>ATHLIMA</h3>
+                    <span>(UKM Olahraga)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/kegiatan-kemahasiswaan-dan-lembaga-semi-otonom"
-            className={classes.link}
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 0.8,
+            }}
           >
-            <MxmHomeCard color="#F4224B" border="#F4224B">
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#FFD008' }}>
-                  DRASTIRIO-
-                  <br />
-                  TITES
-                </h3>
-                <span>
-                  (Kegiatan Kemahasiswaan & Lembaga Semi Otonom)
-                </span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/kegiatan-kemahasiswaan-dan-lembaga-semi-otonom"
+                className={classes.link}
+              >
+                <MxmHomeCard color="#F4224B" border="#F4224B">
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#FFD008' }}>
+                      DRASTIRIO-
+                      <br />
+                      TITES
+                    </h3>
+                    <span>
+                      (Kegiatan Kemahasiswaan & Lembaga Semi Otonom)
+                    </span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/media-kampus"
-            className={classes.link}
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 1,
+            }}
           >
-            <MxmHomeCard color="#F4224B" border="#F4224B">
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#FFD008' }}>MESOU</h3>
-                <span>(Media Kampus)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/media-kampus"
+                className={classes.link}
+              >
+                <MxmHomeCard color="#F4224B" border="#F4224B">
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#FFD008' }}>MESOU</h3>
+                    <span>(Media Kampus)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/komunitas"
-            className={classes.link}
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 1.2,
+            }}
           >
-            <MxmHomeCard>
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#1F2C4C' }}>KOINOTITA</h3>
-                <span>(Komunitas)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/lembaga-kampus"
-            className={classes.link}
-          >
-            <MxmHomeCard>
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#1F2C4C' }}>LYDRYMA</h3>
-                <span>(Lembaga Kampus)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/komunitas"
+                className={classes.link}
+              >
+                <MxmHomeCard>
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#1F2C4C' }}>KOINOTITA</h3>
+                    <span>(Komunitas)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
 
-        <MxmHomeDeckItem>
-          <Link
-            to="/home/organisator/organisasi-dan-himpunan-mahasiswa"
-            className={classes.link}
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 1.4,
+            }}
           >
-            <MxmHomeCard color="#F4224B" border="#F4224B">
-              <div>
-                <span style={{ color: 'white' }}>ARCHI TOU</span>
-                <h3 style={{ color: '#FFD008' }}>ORGANOSI</h3>
-                <span>(Organisasi & Himpunan Mahasiswa)</span>
-              </div>
-            </MxmHomeCard>
-          </Link>
-        </MxmHomeDeckItem>
-      </MxmHomeCardDeck>
-    </Box>
+
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/lembaga-kampus"
+                className={classes.link}
+              >
+                <MxmHomeCard>
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#1F2C4C' }}>LYDRYMA</h3>
+                    <span>(Lembaga Kampus)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
+
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 100,
+              damping: 20,
+              delay: 1.6,
+            }}
+          >  
+            <MxmHomeDeckItem>
+              <Link
+                to="/home/organisator/organisasi-dan-himpunan-mahasiswa"
+                className={classes.link}
+              >
+                <MxmHomeCard color="#F4224B" border="#F4224B">
+                  <div>
+                    <span style={{ color: 'white' }}>ARCHI TOU</span>
+                    <h3 style={{ color: '#FFD008' }}>ORGANOSI</h3>
+                    <span>(Organisasi & Himpunan Mahasiswa)</span>
+                  </div>
+                </MxmHomeCard>
+              </Link>
+            </MxmHomeDeckItem>
+          </motion.div>
+        </MxmHomeCardDeck>
+      </Box>
+    </motion.div>
   );
 };
 
--- src/views/others/JingleMaxima/JingleMaxima.js
+++ src/views/others/JingleMaxima/JingleMaxima.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/src/styles.scss';
+import { motion } from 'framer-motion';
 import './JingleMaxima.scss';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Box } from '@material-ui/core';
@@ -37,65 +38,108 @@ const useStyles = makeStyles({
 const JingleMaxima = () => {
   const classes = useStyles();
   return (
-    <Box style={{ minHeight: '100vh' }}>
-      <Container maxWidth="sm">
-        <Box className={classes.musicbox}>
-          <h1 className="jingle-bersama">BERSAMA</h1>
-          <h1 className="jingle-maxima">MAXIMA</h1>
-          <AudioPlayer
-            className="react-h5-audio-player"
-            src="https://d1z9g6p5mcoq6s.cloudfront.net/assets/misc/jingle_maxima.wav"
-          />
-        </Box>
-        <Box className={classes.lyrics}>
-          <span className={classes.text}>
-            Halo semua
-            <br />
-            Dengkarkan ini <br />
-            Lagu untukmu <br />
-            Untuk langkahmu <br />
-            Ayo resapi sepenuh hati <br />
-            Jalanmu mulai di sini <br />
-            <br />
-            Tak perlu ragu dengan pilihan <br />
-            Kuatkan hatimu hadapi tantangan <br />
-            Bangun jalan baru dengan tujuan <br />
-            Saatnya kau ambil peran <br />
-            <br />
-            Teguhkan diri <br />
-            Satukan hati <br />
-            Langkahmu mulai di sini <br />
-            <br />
-            REFF : <br />
-            Oooo Eeee bersama{' '}
-            <span className="jingle-maxima-highlight">MAXIMA</span>
-            <br />
-            Oooo Eeeee ekspresikan dirimu
-            <br />
-            Ayo mahasiswa, pacu semangatmu <br />
-            Mari kita bangun jalan yang baru <br />
-            Bersama{' '}
-            <span className="jingle-maxima-highlight">
-              MAXIMA
-            </span>{' '}
-            <br />
-            <br />
-            Buka buka kita bukan jalan baru <br />
-            Maju maju ayo kita melangkah maju
-            <br />
-            Teguhkan hatimu, raih impianmu <br />
-            <br />
-            Buka buka kita buka jalan baru <br />
-            Maju maju ayo kita melangkah maju <br />
-            Satukan hatimu, langkah maju, bersama{' '}
-            <span className="jingle-maxima-highlight">MAXIMA</span>
-            <br />
-            <br />
-            REFF
-          </span>
-        </Box>
-      </Container>
-    </Box>
+    <motion.div
+      initial={{ y: -999, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        type: 'spring',
+        stiffness: 50,
+        damping: 20,
+      }}
+    >
+      <Box style={{ minHeight: '100vh' }}>
+        <Container maxWidth="sm">
+          <Box className={classes.musicbox}>
+            <motion.div
+              initial={{ x: -999, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                stiffness: 50,
+                damping: 20,
+                delay: 0.5
+              }}
+            >
+              <h1 className="jingle-bersama">BERSAMA</h1>
+            </motion.div>
+            <motion.div
+              initial={{ x: 999, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                stiffness: 50,
+                damping: 20,
+                delay: 0.5
+              }}
+            >
+              <h1 className="jingle-maxima">MAXIMA</h1>
+            </motion.div>
+            <AudioPlayer
+              className="react-h5-audio-player"
+              src="https://d1z9g6p5mcoq6s.cloudfront.net/assets/misc/jingle_maxima.wav"
+            />
+          </Box>
+          <motion.div
+            initial={{ y: 999, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 50,
+              damping: 20,
+              delay: 0.5
+            }}
+          >  
+            <Box className={classes.lyrics}>
+              <span className={classes.text}>
+                Halo semua
+                <br />
+                Dengkarkan ini <br />
+                Lagu untukmu <br />
+                Untuk langkahmu <br />
+                Ayo resapi sepenuh hati <br />
+                Jalanmu mulai di sini <br />
+                <br />
+                Tak perlu ragu dengan pilihan <br />
+                Kuatkan hatimu hadapi tantangan <br />
+                Bangun jalan baru dengan tujuan <br />
+                Saatnya kau ambil peran <br />
+                <br />
+                Teguhkan diri <br />
+                Satukan hati <br />
+                Langkahmu mulai di sini <br />
+                <br />
+                REFF : <br />
+                Oooo Eeee bersama{' '}
+                <span className="jingle-maxima-highlight">MAXIMA</span>
+                <br />
+                Oooo Eeeee ekspresikan dirimu
+                <br />
+                Ayo mahasiswa, pacu semangatmu <br />
+                Mari kita bangun jalan yang baru <br />
+                Bersama{' '}
+                <span className="jingle-maxima-highlight">
+                  MAXIMA
+                </span>{' '}
+                <br />
+                <br />
+                Buka buka kita bukan jalan baru <br />
+                Maju maju ayo kita melangkah maju
+                <br />
+                Teguhkan hatimu, raih impianmu <br />
+                <br />
+                Buka buka kita buka jalan baru <br />
+                Maju maju ayo kita melangkah maju <br />
+                Satukan hatimu, langkah maju, bersama{' '}
+                <span className="jingle-maxima-highlight">MAXIMA</span>
+                <br />
+                <br />
+                REFF
+              </span>
+            </Box>
+          </motion.div>
+        </Container>
+      </Box>
+    </motion.div>
   );
 };
 
